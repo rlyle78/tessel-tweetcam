29a78d3b54c27dacc144ecd7ff092acbc2018848
Ron Dagdag <rlyle78@gmail.com>
2015-12-20 04:53:11 -0600

    Update tweet.js

--- climate/tweet.js
+++ climate/tweet.js
@@ -9,9 +9,9 @@ var status = 'Hello ' + twitterHandle + '. This is your #Tessel speaking.';
 // Enter the oauth key and secret information
 var twit = new twitter({
   consumer_key: 'O7oc0pvsZn4xjgcuHuYdX4FaC',
-  consumer_secret: 'iJYuHFz2sD46Nvk3mcwzX8uih14aEAMgVWdWoR59nx8v6Zl7ZX',
-  access_token_key: '2529232909-luARGU89K4CKFMvfzBjCgG6ubefzDkdDWkSB85i',
-  access_token_secret: 'GXQfuzvGdjLEs3t1HEYfhQ9x9bdBcSBVXjBkbRgwYlOE0'
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
 
 // Make a tweet!
